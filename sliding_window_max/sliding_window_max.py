2575b5fe2e85107f45b4a385c85369973f997e85
janecyyu <janecyyu@gmail.com>
2020-06-18 14:30:11 -0700

    moving zero updated

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,24 +4,6 @@ Returns: a List of integers
 '''
 
 
-# def sliding_window_max(numbers, k):
-#     # Your code here
-#     res = []
-#     find_max = numbers[0]
-#     # find the first max
-#     if k == 1:
-#         res.append(find_max)
-#     if k > 1:
-#         for i in range(1, k):
-#             find_max = max(find_max, numbers[i])
-#         res.append(find_max)
-
-#     for i in range(k, len(numbers)):
-#         find_max = max(find_max, numbers[i])
-#         res.append(find_max)
-#     return res
-
-
 def sliding_window_max(numbers, k):
     def get_window_max(start_idx):
         window_max = float('-inf')
@@ -29,7 +11,6 @@ def sliding_window_max(numbers, k):
             window_max = max(window_max, numbers[i])
         return window_max
 
-    # Your code here
     res = []
 
     for i in range(len(numbers) - k + 1):
