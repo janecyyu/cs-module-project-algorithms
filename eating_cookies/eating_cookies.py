9fd470661542d6cec7eac53b166e895b0bbf9c00
janecyyu <janecyyu@gmail.com>
2020-06-17 20:23:18 -0700

    eat cookies ok

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,26 @@
 Input: an integer
 Returns: an integer
 '''
+
+
 def eating_cookies(n):
     # Your code here
+    if n <= 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
 
-    pass
+    eat_one_cookie_first = eating_cookies(n - 1)
+    eat_two_cookies_first = eating_cookies(n - 2)
+    eat_three_cookies_first = eating_cookies(n - 3)
+    return eat_one_cookie_first + eat_two_cookies_first + eat_three_cookies_first
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
