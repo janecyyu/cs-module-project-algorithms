398e4441d1c035d271b7690cec702ab439d4330c
janecyyu <janecyyu@gmail.com>
2020-06-18 15:29:11 -0700

    product of all  updated

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -7,10 +7,32 @@ import math
 
 def product_of_all_other_numbers(arr):
     # Your code here
-    sum = math.prod(arr)
-    for i in range(len(arr)):
-        arr[i] = sum / arr[i]
-    return arr
+    # sum = math.prod(arr)
+    # for i in range(len(arr)):
+    #     arr[i] = sum / arr[i]
+    # return arr
+
+    length = len(arr)
+    # create two array
+    L = []
+    R = []
+
+    left = 1
+    # counting from left to the end of array
+    for i in range(length):
+        L.append(left)
+        left *= arr[i]
+
+    right = 1
+    # counting from right to the begining of array
+    for i in reversed(range(length)):
+        R.insert(i, right)
+        # update L array
+        L[i] = L[i]*right
+        right *= arr[i]
+    return L
+
+    # ^^^ runtime O(n), nno division
 
 
 if __name__ == '__main__':
